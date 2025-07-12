c5fe008974d77a0986089b727f75f97e40e1d225
Rory Roy Ulloa <rulloa1@my.devry.edu>
2025-07-12 16:19:31 -0500

    Delete my-app directory

--- my-app/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",   // enables `next export`
-  trailingSlash: true // friendlier for Netlify's static hosting
-};
-
-export default nextConfig;
